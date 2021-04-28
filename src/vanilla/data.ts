af51b5aeb6e4c8c982d6470620ef546bf6738620
alyssagaudioso <alyssa.gaudioso@gmail.com>
2021-04-27 23:45:22 -0700

    ep 3

--- src/vanilla/data.ts
+++ src/vanilla/data.ts
@@ -1,6 +1,10 @@
 import { Bitch, Fuckboi, Couple, Ceremony } from "./types";
 
-export const noMatchPairs: string[] = [`${Bitch.Jessica}_${Fuckboi.Brandon}`];
+export const noMatchPairs: string[] = [
+  `${Bitch.Jessica}_${Fuckboi.Brandon}`,
+  `${Bitch.Christina}_${Fuckboi.Brandon}`,
+  `${Bitch.Alexandria}_${Fuckboi.Brandon}`,
+];
 
 export const perfectMatches: string[] = [];
 
@@ -10,14 +14,44 @@ export const ceremonies: Ceremony[] = [
     couples: [
       `${Bitch.Jenni}_${Fuckboi.Layton}`,
       `${Bitch.Ashley}_${Fuckboi.Dario}`,
-      `${Bitch.Jenni}_${Fuckboi.Layton}`,
       `${Bitch.Paris}_${Fuckboi.Pratt}`,
       `${Bitch.Briana}_${Fuckboi.Curtis}`,
-      `${Bitch.Christina}_${Fuckboi.Brandon}`,
       `${Bitch.Jasmine}_${Fuckboi.John}`,
-      `${Bitch.Ellie}_${Fuckboi.Alex}`,
+      `${Bitch.Christina}_${Fuckboi.Brandon}`,
+      `${Bitch.Alexandria}_${Fuckboi.Garland}`,
       `${Bitch.Jessica}_${Fuckboi.Anthony}`,
-      `${Bitch.Alexandria}_${Fuckboi.Nathan}`,
+      `${Bitch.Shelby}_${Fuckboi.Nathan}`,
+      `${Bitch.Ellie}_${Fuckboi.Alex}`,
+    ],
+  },
+  {
+    numberOfMatches: 2,
+    couples: [
+      `${Bitch.Jessica}_${Fuckboi.Layton}`,
+      `${Bitch.Shelby}_${Fuckboi.Dario}`,
+      `${Bitch.Paris}_${Fuckboi.Pratt}`,
+      `${Bitch.Jasmine}_${Fuckboi.Garland}`,
+      `${Bitch.Ashley}_${Fuckboi.John}`,
+      `${Bitch.Tyler}_${Fuckboi.Brandon}`,
+      `${Bitch.Christina}_${Fuckboi.Alex}`,
+      `${Bitch.Alexandria}_${Fuckboi.Anthony}`,
+      `${Bitch.Jenni}_${Fuckboi.Nathan}`,
+      `${Bitch.Briana}_${Fuckboi.Curtis}`,
+    ],
+  },
+  {
+    numberOfMatches: 3,
+    couples: [
+      `${Bitch.Christina}_${Fuckboi.John}`,
+      `${Bitch.Briana}_${Fuckboi.Curtis}`,
+      `${Bitch.Jessica}_${Fuckboi.Nathan}`,
+      `${Bitch.Shelby}_${Fuckboi.Brandon}`,
+      `${Bitch.Paris}_${Fuckboi.Pratt}`,
+      `${Bitch.Alexandria}_${Fuckboi.Garland}`,
+      `${Bitch.Ashley}_${Fuckboi.Dario}`,
+      `${Bitch.Jasmine}_${Fuckboi.Alex}`,
+      `${Bitch.Jenni}_${Fuckboi.Anthony}`,
+      `${Bitch.Ellie}_${Fuckboi.Layton}`,
     ],
   },
 ];
